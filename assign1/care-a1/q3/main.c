2340fd781d94bc75511d4145c16ce70736b096f7
Wallman <daniel.wallman94@gmail.com>
2019-01-14 13:54:43 -0800

    add assign1

---
+++ assign1/care-a1/q3/main.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int mystery(char *, int);
+
+char *str = "Good mood food.";
+
+void main () {
+    int n = 15;
+    printf("The return value was:  %d.\n", mystery(str,n));
+    return;
+}
