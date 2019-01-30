56e42047df1c83839abed46d34fd0bc8a3068bcd
Wallman <daniel.wallman94@gmail.com>
2019-01-30 10:03:48 -0800

    add lab 4, start assign3

--- assign3/care-a3/conv.s
+++ assign3/care-a3/conv.s
@@ -1,3 +1,18 @@
-	.globl conv
-conv:
+# %rdi = signal[]
+# %rsi = h[]
+# %edx = length of arrs
+# %eax = sum
+# %ecx = counter
+
+	.globl _conv
+_conv:
+	movl $0, %eax
+	movl $0, %ecx
+
+loop:	cmpl %ecx, %edx
+	jle end
+	leaq (%rsi,
+	imul (%rdi, %ecx, 8), (%rsi,
+
+end:	
 	ret
