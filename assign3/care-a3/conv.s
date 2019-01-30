9f5805cdf1e2101db758dd8757a80816b7549165
Wallman <daniel.wallman94@gmail.com>
2019-01-30 15:22:40 -0800

    conv done?

--- assign3/care-a3/conv.s
+++ assign3/care-a3/conv.s
@@ -1,26 +1,28 @@
-# %edi = signal[]
-# %esi = h[]
-# %edx = length of arrs
-# %eax = sum
-# %ecx = loop counter
-# %r8d = h counter
-# %r9d = temp
+# %edi  = signal[]
+# %esi  = h[]
+# %edx  = length of arrs
+# %eax  = sum
+# %ecx 	= loop counter
+# %r8d  = h counter
+# %r9d 	= temp sum
+# %r10d = negation of ecx
 
-	.globl _conv
-_conv:
+	.globl conv
+conv:
 	movl $0, %eax
 	movl $0, %ecx
-	leal (%esi, %edi, 8), %r8d
+	leal (%esi, %edx, 8), %r8d	# r8d = adress of h[n-1]
 
 loop:	cmpl %ecx, %edx
 	je end
 
-	movl %ecx, %r10d		#negate ecx for backwards loop
+	movl %ecx, %r10d		# negate ecx for backwards loop
 	neg %r10d
 
-	movl (%edx, %ecx, 8), %r9d
-	imull (%r8d, %r10d, 8), %r9d
-	addl %r9d, %eax
+	movl (%edi, %ecx, 8), %r9d	# r9d = signal[m]
+	imull (%r8d, %r10d, 8), %r9d	# r9d *= h[n-1-m]
+	addl %r9d, %eax			# eax = r9d
+
 	incl %ecx
 	jmp loop
 end:	
