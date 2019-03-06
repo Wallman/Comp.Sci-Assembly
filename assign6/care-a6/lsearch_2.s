87c328bba61b5e8d5aa07d4a2c6755f8d4343c94
Wallman <daniel.wallman94@gmail.com>
2019-03-06 10:45:53 -0800

    assign6 3(c) done

--- assign6/care-a6/lsearch_2.s
+++ assign6/care-a6/lsearch_2.s
@@ -1,43 +1,43 @@
 	# Variable map
-	# %rdi	first arg
-	# %esi	second arg (string?)
-	# %r9d	
-	# %edx	
-	# %ecx	
-	# %r8d	
+	# %rdi	int *A
+	# %esi	int n
+	# %edx	int target
+	# %ecx	counter
+	# %r8d	tmpCounter
+	# %r9d	tmp
 	# %rax	result
 
 
 	.globl	lsearch_2
 lsearch_2:
-	testl	%esi, %esi			# if esi <= 0 goto error
+	testl	%esi, %esi				# if n <= return -1
 	jle	error
-	movslq	%esi, %rax			# move string? rax <- esi
+	movslq	%esi, %rax
 	leaq	-4(%rdi,%rax,4), %rax
-	movl	(%rax), %r9d
-	movl	%edx, (%rax)
-	cmpl	(%rdi), %edx
-	je	.L7
-	movl	$1, %ecx
-.L5:
+	movl	(%rax), %r9d			# tmp = A[n-1]
+	movl	%edx, (%rax)			# A[n-1] = target
+	cmpl	(%rdi), %edx			# if A[0]==target goto izero
+	je	izero
+	movl	$1, %ecx				# counter = 1
+loop:
 	movl	%ecx, %r8d
-	addq	$1, %rcx
-	cmpl	%edx, -4(%rdi,%rcx,4)
-	jne	.L5
-.L4:
-	movl	%r9d, (%rax)
+	addq	$1, %rcx				# i++
+	cmpl	%edx, -4(%rdi,%rcx,4)	# while(A[i] != target)
+	jne	loop
+conditionals:
+	movl	%r9d, (%rax)			# A[n-1] = tmp
 	leal	-1(%rsi), %eax
-	cmpl	%r8d, %eax
-	jg	.L8
-	cmpl	%edx, %r9d
-	jne	.L6
-	rep ret
-.L8:
-	movl	%r8d, %eax
+	cmpl	%r8d, %eax				# if i < n-1 return i
+	jg	returni
+	cmpl	%edx, %r9d				# if target != tmp return -1
+	jne	error
+	rep ret 						# return n-1
+returni:
+	movl	%r8d, %eax				# result = tmp_counter
 	ret
-.L7:
-	xorl	%r8d, %r8d
-	jmp	.L4
+izero:
+	xorl	%r8d, %r8d				# tmp_counter = 0
+	jmp	conditionals
 error:
-	movl	$-1, %eax
+	movl	$-1, %eax				# return -1
 	ret
